1c36bc1a0fda598100e69c2446cc9be3cc6877e8
Oswin Bult <oswin.bult@live.nl>
2022-08-02 15:00:43 +0200

    first try implementing the basic ClapTrap

---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,24 @@
+#ifndef CLAP_TRAP_HPP
+# define CLAP_TRAP_HPP
+
+class	ClapTrap
+{
+private:
+	std::string		_name;
+	int				_hp;
+	int				_energy;
+	int				_attackDamage;
+public:
+	ClapTrap();
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap& ref);
+	ClapTrap& operator=(const ClapTrap& ref);
+	~ClapTrap();
+
+	std::string	getName();
+	void	attack(const std::string& target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+};
+
+#endif
